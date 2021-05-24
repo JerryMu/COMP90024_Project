c3ab9410ccbc2a70eb29ad1b9b96c8809cb21457
Pengyu <Jerrymupengyu@gmail.com>
2021-05-24 16:59:33 +0800

    flask try

--- FlaskWebServer/flaskr/fetchCouchDB.py
+++ FlaskWebServer/flaskr/fetchCouchDB.py
@@ -1,20 +1,12 @@
 import couchdb2
-import pandas as pd
-master_node = 'http://admin:admin@172.26.128.217:5984/'
-couch = couchdb2.Server(master_node)
-tweet_db = couch['tweet']
+from flask import current_app, g
+from flask.cli import with_appcontext
+
+MASTER_NODE = 'http://admin:admin@172.26.128.217:5984/'
 
 
-"""    
-def view(self, designname, viewname,
-     key=None, keys=None, startkey=None, endkey=None,
-     skip=None, limit=None, sorted=True, descending=False,
-     group=False, group_level=None, reduce=None, include_docs=False,
-     update=None):
-"""
-
-rows = tweet_db.view("tweetDesign", "get_basic_data", include_docs=True)
-
-data = [row['doc'] for row in rows]
-df = pd.DataFrame(data)
-print('finish')
+def get_db(db_name):
+    server = couchdb2.Server(MASTER_NODE)
+    if db_name not in g:
+        g.db_name = server[db_name]
+    return g.db_name
--- FlaskWebServer/flaskr/index.py
+++ FlaskWebServer/flaskr/index.py
@@ -2,6 +2,14 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
+from flaskr.fetchCouchDB import get_db
+
+CITIES = ["Sydney",
+          "Melbourne",
+          "Brisbane",
+          "Perth (WA)",
+          "Adelaide"]
+
 bp = Blueprint('main', __name__, url_prefix='/main')
 
 
@@ -19,7 +27,14 @@ def welcome_page():
 
 @bp.route('/scenario_1', methods=['GET', 'POST'])
 def scenario_1():
-    return render_template('scenario-1.html')
+    scenario_1_db = get_db('scenario_1')
+    # Total number of tweet mentioned COVID-19.
+    tweet_mentions = scenario_1_db['ct_attention_covid']
+    data = {
+        'tweet_mentions_key': list(tweet_mentions.keys())[2:],
+        'tweet_mentions_value': list(tweet_mentions.values())[2:]
+    }
+    return render_template('Scenario-1.html', data=data)
 
 
 @bp.route('/scenario_2', methods=['GET', 'POST'])
@@ -40,3 +55,7 @@ def scenario_4():
 @bp.route('/scenario_5', methods=['GET', 'POST'])
 def scenario_5():
     return render_template('scenario-5.html')
+
+@bp.route('/page_1', methods=['GET', 'POST'])
+def page_1():
+    return render_template('Page-1.html')
